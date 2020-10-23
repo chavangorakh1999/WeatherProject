4341a775ffd4d913e79bf5a15367aec19f44d358
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-24 00:19:05 +0530

    sending required data to website

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ const app= express();
 app.get("/",function(req,res){
     const url="https://api.weatherapi.com/v1/current.json?key=c35855c5c3e241cfbc152706201910&q=Pune"
     https.get(url,function(response){
-        console.log(response.statusCode);
         response.on("data",function(data){
             const weatherData= JSON.parse(data);
             const temp= weatherData.current.temp_c;
             const weatherDescription=weatherData.current.condition.text
+            
             console.log(temp);
             console.log(weatherDescription);
+            res.write("<p>Possibility of "+weatherDescription+"</p>");
+            res.write("<h1>Temprature in Pune is "+temp+" Degree Celcius.</h1>");
+            res.send();
+
         })
     });
     
-    res.send("Server Is Running");
 });
 
 
