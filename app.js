393b1639806638ddca286f1b0827101e8654cafe
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-24 10:32:20 +0530

    done with customised weather web app.

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express= require("express");
 const https=  require ("https");
+const bodyParser = require("body-parser"); 
 
 const app= express();
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-    const url="https://api.weatherapi.com/v1/current.json?key=c35855c5c3e241cfbc152706201910&q=Pune"
+    res.sendFile(__dirname+"/index.html");
+    
+});
+app.post("/",function(req,res){
+   const apiKey="113a78f5ca86da9fdd6d9720ef81af47";
+    const query=req.body.cityName;
+    const unit="metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units=" + unit + "&appid="+apiKey;
     https.get(url,function(response){
         response.on("data",function(data){
             const weatherData= JSON.parse(data);
-            const temp= weatherData.current.temp_c;
-            const weatherDescription=weatherData.current.condition.text
-            
-            console.log(temp);
-            console.log(weatherDescription);
+            const temp=weatherData.main.temp;
+            const weatherDescription=weatherData.weather[0].description;
+            const icon=weatherData.weather[0].icon;
             res.write("<p>Possibility of "+weatherDescription+"</p>");
-            res.write("<h1>Temprature in Pune is "+temp+" Degree Celcius.</h1>");
-            res.send();
+            res.write("<img src='https://openweathermap.org/img/wn/"+icon+"@2x.png'></img>")
+            res.write("<h1>Temprature in "+ query +" is "+temp+" Degree Celcius.</h1>");
+    res.send();
 
         })
     });
-    
 });
 
+    
 
 
 app.listen(3000,function(req,res){
